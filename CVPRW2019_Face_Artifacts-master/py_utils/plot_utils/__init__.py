9e4af0709b8718c5bb3363e920120358d41a587a
Emre Kavak <emrekavak@EK-MBPro.local>
2021-05-24 16:52:41 +0200

    remove from master

--- CVPRW2019_Face_Artifacts-master/py_utils/plot_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Proj: PyCVApp
-Date: 4/13/18
-Written by Yuezun Li
---------------------------
-"""
--- CVPRW2019_Face_Artifacts-master/tf_utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Proj: resolution-net
-Date: 7/28/18
-Written by Yuezun Li
---------------------------
-"""
