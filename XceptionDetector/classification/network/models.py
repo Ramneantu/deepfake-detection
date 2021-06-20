227579c19537c5db9b106b7092ccaf0b9ade4eb9
Emre Kavak <emrekavak@h157.tum.vpn.lrz.de>
2021-06-20 14:56:05 +0200

    import

--- XceptionDetector/classification/network/models.py
+++ XceptionDetector/classification/network/models.py
@@ -10,7 +10,7 @@ import torch
 import pretrainedmodels
 import torch.nn as nn
 import torch.nn.functional as F
-from network.xception import xception
+from XceptionDetector.classification.network import xception
 import math
 import torchvision
 
