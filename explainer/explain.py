d7df687230bbf1dd95bc474f8e81c8c7d243a98d
Emanuel Ramneantu <emanuel.ramneantu@tum.de>
2021-06-24 14:12:19 +0200

    changes regarding gpu

--- explainer/explain.py
+++ explainer/explain.py
@@ -94,7 +94,7 @@ def explain(datasetName, modelNameSpecs, batch_predict, oneIsFake=True):
                                                         top_labels=2,
                                                         hide_color=0,
                                                         num_samples=1500,
-                                                        batch_size=32)
+                                                        batch_size=2)
             temp, mask = explanation.get_image_and_mask(0, positive_only=False, num_features=10,
                                                         hide_rest=False)
             img_boundary = mark_boundaries(temp / 255.0, mask)
@@ -107,7 +107,7 @@ def explain(datasetName, modelNameSpecs, batch_predict, oneIsFake=True):
             else:
                 classifiedAs = str(kind_val == explainedClass) + ("Real" if kind == "fake" else "Fake")
 
-    plt.imsave(os.path.join(datasetName, modelNameSpecs, classifiedAs, filename), img_boundary)
+            plt.imsave(os.path.join(datasetName, modelNameSpecs, classifiedAs, filename), img_boundary)
 
 
 def explainExistingModels(load_model, model_batch_predict):
