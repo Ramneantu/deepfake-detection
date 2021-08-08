09d7ca8bc0203e7523a3df92192da4c84b061fcc
Emanuel Ramneantu <emanuel.ramneantu@tum.de>
2021-08-08 13:50:44 +0200

    script

--- classification/run_all.sh
+++ classification/run_all.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# nohup python classify_exception.py -d X-ray -e 30 --early_stopping 5 -bs 32
+nohup python classify_xception.py -d X-ray -l -e 30 --early_stopping 10 --lr_decay 3 -bs 32
 
-# nohup python classify_exception.py -d HQ -e 30 --early_stopping 5 -bs 32
+nohup python classify_xception.py -d HQ -l -e 30 --early_stopping 5 -bs 32
 
-# nohup python classify_exception.py -d FF-raw -e 30 --early_stopping 5 -bs 32
+nohup python classify_xception.py -d FF-raw -l -e 30 --early_stopping 5 -bs 32
 
-# nohup python classify_exception.py -d FF-compressed -e 30 --early_stopping 5 -bs 32
-
-nohup python classify_exception.py -d X-ray -l -e 30 --early_stopping 10 --lr_decay 3 -bs 32
-
-nohup python classify_exception.py -d HQ -l -e 30 --early_stopping 5 -bs 32
-
-nohup python classify_exception.py -d FF-raw -l -e 30 --early_stopping 5 -bs 32
-
-nohup python classify_exception.py -d FF-compressed -l -e 30 --early_stopping 5 -bs 32
+nohup python classify_xception.py -d FF-compressed -l -e 30 --early_stopping 5 -bs 32
 
